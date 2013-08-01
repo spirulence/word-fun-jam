cdc2fddd896be91a4aaceb751f15fbdb1cb42fff
spirulence <spirulence@gmail.com>
2013-08-01 00:49:54 -0700

    game initialization should work

--- server.js
+++ server.js
@@ -3,37 +3,119 @@ var http = require("http");
 var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
+  , url = require('url')
 
-var words = fs.readFileSync("brit-a-z.txt", {encoding:"utf-8"})
-words = words.replace("'", "").replace("\r", "").split("\n")
-
-var games = {}
-	
-function validName(name){
-    return /\w+/.test(name)
-}
+var words = fs.readFileSync("brit-a-z.txt", {encoding:"utf-8"});
+words = words.replace("'", "").replace("\r", "").split("\n");
 
 app.listen(8765);
 
-function handler (req, res) {
-  fs.readFile(__dirname + req['url'],
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading');
-    }
+var games = {};
 
-    res.writeHead(200);
-    res.end(data);
-  });
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+    
+function Game(name){
+    this.name = name;
+    this.inProgress = false;
+    this.players = []
+    this.nicks = []
+}
+Game.prototype = {
+    name: null,
+    join: function(socket){
+        if(this.nicks.indexOf(socket.nick) == -1 && this.nicks.length < 2){
+            this.players.forEach( function(player){
+                socket.emit('setOpponent', player.nick);
+            });
+            
+            socket.game = game;
+            socket.score = 0;
+            this.players.push(socket);
+            this.nicks.push(socket.nick);
+            
+            if(this.nicks.length == 2){
+                this.players.forEach( function(player){
+                    player.emit('start');
+                });
+                this.inProgress = true;
+            }
+            
+            return true;
+        }
+        return false;
+    }
+    hasNick: function(nick){
+        return this.nicks.indexOf(nick) != -1;
+    }
+}
+    
+function validGameId(name){
+    return /\w+/.test(name)
+}
+
+function nickAvailable(gameId, nick){
+    if(games.hasOwnProperty(gameId)){
+        if(games[gameId].hasNick(nick)){
+            return false;
+        }else{
+            return true;
+        }
+    }else{
+        return true;
+    }
+}
+    
+function handler (req, res) {
+    if(req['url'].startswith("/game/")){
+        var parsed = url.parse(req['url'].slice(5));
+        var splits = parsed.path.split("/");
+        if(splits.length == 2){
+            var gameId = splits[0];
+            var nick = splits[1];
+            if(validGameId(gameId) && nickAvailable(gameId, nick)){
+                fs.readFile(__dirname + "/game.html",
+                function (err, data) {
+                    if (err) {
+                        res.writeHead(500);
+                        res.end("Error loading");
+                    }
+                    res.writeHead(200);
+                    res.end(data);
+                });
+            }
+        }
+    }else{
+        if(req['url'] == "/"){
+            req['url'] = "/index.html";
+        }
+        fs.readFile(__dirname + req['url'],
+        function (err, data) {
+            if (err) {
+                res.writeHead(500);
+                res.end("Error loading");
+            }
+
+            res.writeHead(200);
+            res.end(data);
+        });
+    }
 }
 
 
-
 io.sockets.on('connection', function (socket) {
-  socket.on('chatmessage', function (data) {
-        //data.clientid = this.id;
-        //io.sockets.emit('chatmessage', data);
-        socket.broadcast.emit('chatmessage', data);
-  });
+    socket.on('init', function (gameId, nick){
+        socket.nick = nick;
+        
+        if(!games.hasOwnProperty(gameId)){
+            games[gameId] = new Game(gameId);
+        }
+        var game = games[gameId];
+        
+        if (game.join(socket)){
+            socket.emit('initResponse', {});
+        }
+    });
+    
 });
