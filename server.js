ccf178695822e5dd34433b6273ffb960720e89c5
Tyguy7 <Tyguy7@gmail.com>
2013-12-31 17:47:04 -0800

    Narrowly saved my rebuild from git stash

--- server.js
+++ server.js
@@ -1,27 +1,325 @@
-var http = require("http");
-
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , _ = require('lodash')
-  , fs = require('fs')
-  , url = require('url');
+//set up some server globals
+var liveUsers = {};
+var games = {};
+var expiredGames = {};
+var port = process.env.PORT || 8765;
 
 var words = fs.readFileSync("brit-a-z.txt");
 words = words.toString('utf8');
 words = words.replace("'", "").replace("\r", "").split("\n");
 
-var smallwords = fs.readFileSync("smallwords.txt");
-smallwords = smallwords.toString('utf8');
-smallwords = smallwords.replace("'", "").replace("\r", "").split("\n");
+var smallWords = fs.readFileSync("smallwords.txt");
+smallWords = smallWords.toString('utf8');
+smallWordsƒ = smallWords.replace("'", "").replace("\r", "").split("\n");
 
-var port = process.env.PORT || 8765;
+//http request handler
+function handler (req, res) 
 
-app.listen(port);
+    //user login
+    if(req['url'] == "/login"){
 
-var games = {};
+        //check username / pass are alphanumeric
+        var rx = '???????';
+        if(rx.check(username) || rx.check(password) ) {
 
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+            data = JSON.stringify({message:'username and password must be alphanumeric'});
+            res.setHeader("Content-Type", "text/json");
+            res.writeHead(403);
+            res.end(data);
+            return;
+
+        } else {
+
+            //read in user data
+            var users = fs.readFileSync("users.json");
+
+            //if the file is locked by another user, try unti its not
+            var tries = 1;
+            while (users=='locked' && tries <= 1000) {
+                var users = fs.readFileSync("users.json");
+                tries++;
+            }
+
+            //or fail
+            if (users=='locked' && tries >= 1000) {
+                data = JSON.stringify({message:'server error, could not read user database'});
+                res.setHeader("Content-Type", "text/json");
+                res.writeHead(500);
+                res.end(data);
+                return;
+            }
+
+            //if we can parse the JSON, make sure to lock the users data file so no one else can use it until its available
+            if (users = JSON.parse(users)){
+
+                //if user exists
+                if (users[username]){
+
+                    //and password is correct, log them in
+                    if ( bcrypt.compareSync(users[username].password, password) ){
+
+
+                        //if user is currently logged in, close their games, and reinitialize everything before logging them in
+                        if (liveUsers[username]){
+
+                            //do that logic here
+                        }
+
+                        //log them in, session-ish
+                        liveUsers[username] = {status:'logged in'};
+
+                        data = JSON.stringify({message:'user logged in'});
+                        res.setHeader("Content-Type", "text/json");
+                        res.writeHead(200);
+                        res.end(data);
+                        
+                    } else {
+                        //bad password
+                        data = JSON.stringify({message:'bad username or password'});
+                        res.setHeader("Content-Type", "text/json");
+                        res.writeHead(401);
+                        res.end(data);
+                    }
+
+                //user is new, add them
+                } else {
+
+                    //TODO: make this right!
+                    fs.writeFileSync("users.json", 'locked');
+
+                    //user doesn't exist, log them in, session-ish, save their info
+                    liveUsers[username] = {status:'logged in'};
+                    //TODO: do this right
+                    users[username] = {password: bcrypt(password, 10)};
+
+                    //'unlock' the user db
+                    //TODO: make this right!
+                    fs.writeFileSync("users.json", JSON.stringify(users));
+
+                    data = JSON.stringify({message:'user logged in'});
+                    res.setHeader("Content-Type", "text/json");
+                    res.writeHead(200);
+                    res.end(data);
+                }
+
+            //failed parsing of json data
+            } else {
+
+                data = JSON.stringify({message:'server error, could not read user database'});
+                res.setHeader("Content-Type", "text/json");
+                res.writeHead(401);
+                res.end(data);
+                return;
+            }
+        }
+
+    //send a list of games that are on the server
+    } else if(req['url'] == "/getgames"){
+
+        var data = [];
+        _.forEach(games, function(game){ 
+            var players=[];
+            _.forEach(game.players, function(player){
+                var obj = {nick: player.nick, score: game.scores[player.nick].score}
+                players.push(obj);
+            });
+            data.push({
+                name: game.name,
+                rounds: game.rounds,
+                players: players
+            });
+        });
+
+        data = JSON.stringify(data);
+        res.setHeader("Content-Type", "text/json");
+        res.writeHead(200);
+        res.end(data);
+    
+    //send static pages
+    } else { 
+        if(req['url'] == "/"){
+            req['url'] = "/index.html";
+        }
+        if(req['url'] == "/join"){
+            req['url'] = "/join.html";
+        }
+        if(req['url'] == "/game"){
+            req['url'] = "/game.html";
+        }
+        fs.readFile(__dirname + req['url'],
+            function (err, data) {
+                if (err) {
+                    res.writeHead(500);
+                    res.end("Error loading");
+                } else {
+                
+                    res.setHeader("Content-Type", contentType(req['url']));
+                    res.writeHead(200);
+                    res.end(data);
+                }
+            }
+        );
+    }
+}
+
+//set up content types
+function contentType(fn) {
+
+    var a = fn.split(".");
+    if( a.length === 1 || ( a[0] === "" && a.length === 2 ) ) {
+        return "";
+    }
+    fn = a.pop().toLowerCase();
+
+    switch(fn)
+    {
+    case 'html':
+      return 'text/html';
+      break;
+    case 'htm':
+      return 'text/html';
+      break;
+    case 'ico':
+      return 'image/x-icon';
+      break;
+    case 'css':
+      return 'text/css';
+      break;
+    case 'js':
+      return 'text/javascript';
+      break;
+    case 'txt':
+      return 'text/plain';
+      break;
+    case 'jpg':
+      return 'image/jpeg';
+      break;
+    case 'jpeg':
+      return 'image/jpeg';
+      break;
+    case 'png':
+      return 'image/png';
+      break;
+    case 'gif':
+      return 'image/gif';
+      break;
+    case 'svg':
+      return 'image/svg+xml';
+      break;
+    default:
+      return 'text/plain';
+    }
+}
+
+//create server
+var app = require('http').createServer(handler),
+    io = require('socket.io').listen(app), 
+    _ = require('lodash'), 
+    fs = require('fs'), 
+    url = require('url');
+
+//websocket functions
+io.sockets.on('connection', function (socket) {
+
+    //set up socket after login
+    socket.on('loggedIn', nick){
+        socket.nick = nick;
+    }
+
+    //create game
+    socket.on('createNewGame', function (gameId, gameData) {
+
+        if(!gameId) socket.emit('error', 'bad game name');
+        var difficulty = gameData['difficulty'] || null;
+        var varyingLevels = gameData['varyingLevels'] || false;
+        var maxPlayers = gameData['maxPlayers'] || 1;
+        var ai = gameData['ai'] || false;
+
+        //if they've chosen a computer player, ignore multiplayer settings if they snuck in
+        if(ai){
+            maxPlayers = 1;
+            varyingLevels = false;
+        }
+
+        //if this game name isn't being used
+        if(validGameId(gameId)){
+
+            //create the game!
+            console.log('sent init data', data);
+            var gameId = data.gameId;
+            var nick = data.nick;
+            socket.nick = nick;
+            var difficulty = null;
+            var ai = data.ai;
+            var speed = data.speed;
+            if(data.difficulty!=null){
+                var difficulty = data.difficulty;
+            }
+            
+            if(!games.hasOwnProperty(gameId)){
+                games[gameId] = new Game(gameId);
+            }
+            var game = games[gameId];
+            
+            if (game.join(socket, gameId, difficulty, ai, speed)){
+                console.log(nick+' joined game '+gameId);
+                var opponent;
+                if(game.players.length==2) { 
+                    console.log(game.players);
+                    game.players.forEach( function(player){
+                        
+                        if(player.id!=socket.id){
+                            opponent = socket.nick;
+                            player.emit('opponentJoined', opponent);
+                            socket.emit('initResponse', nick, player.nick);
+                            game.start();
+                        }
+                    });
+                } else if(game.ai == true) { 
+                    console.log(game.players);
+                    socket.emit('initResponse', nick, 'computer');
+                    game.start();
+
+                } else {
+                    socket.emit('initResponse', nick, opponent);
+                }
+            }            
+        }
+    }
+
+    socket.on('ask4word', function(){
+        socket.emit('wordResponse', getRandomWord());
+    });
+
+    socket.on('keyStroke', function(userText){
+        socket.game.emit(socket, 'update', userText.text);
+    });
+
+    socket.on('submitWord', function(word){
+        if(socket.game.currentWord==word){
+            socket.game.scores[socket.nick].score++;
+            socket.game.newRound(socket.nick);
+        }else{
+            //socket.emit(socket, 'wrong');
+        }
+    });
+}
+
+//some important functions
+function validGameId(gameId){
+    if(/\w+/.test(gameId) && !games[gameId]){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function getRandomWord(gameDifficulty) {
+    if(gameDifficulty=='easy'){
+        return smallWords[Math.floor(Math.random()*smallWords.length)];
+    } else if(gameDifficulty=='normal'){
+        return words[Math.floor(Math.random()*words.length)];
+    }
 }
     
 function Game(name){
@@ -30,6 +328,8 @@ function Game(name){
     this.players = [];
     this.nicks = [];
 }
+
+//our game object.  This guy does all the thinking.
 Game.prototype = {
     name: null,
     scores: {},
@@ -207,211 +507,7 @@ Game.prototype = {
     }
 };
     
-function validGameId(gameId){
-    return /\w+/.test(gameId);
-}
 
-function nickAvailable(gameId, nick){
-    if(games.hasOwnProperty(gameId)){
-        if(games[gameId].hasNick(nick)){
-            return false;
-        }else{
-            return true;
-        }
-    }else{
-        return true;
-    }
-}
+//start the server!
+app.listen(port);
 
-function contentType(fn) {
-
-    var a = fn.split(".");
-
-    if( a.length === 1 || ( a[0] === "" && a.length === 2 ) ) {
-        return "";
-    }
-    fn = a.pop().toLowerCase();
-
-    switch(fn)
-    {
-    case 'html':
-      return 'text/html';
-      break;
-    case 'htm':
-      return 'text/html';
-      break;
-    case 'ico':
-      return 'image/x-icon';
-      break;
-    case 'css':
-      return 'text/css';
-      break;
-    case 'js':
-      return 'text/javascript';
-      break;
-    case 'txt':
-      return 'text/plain';
-      break;
-    case 'jpg':
-      return 'image/jpeg';
-      break;
-    case 'jpeg':
-      return 'image/jpeg';
-      break;
-    case 'png':
-      return 'image/png';
-      break;
-    case 'gif':
-      return 'image/gif';
-      break;
-    case 'svg':
-      return 'image/svg+xml';
-      break;
-    default:
-      return 'text/plain';
-    }
-}
-
-function handler (req, res) {
-    if(/\/game\/.*/.test(req['url'])){
-        var parsed = url.parse(req['url'].slice(5));
-        var splits = parsed.path.split("/");
-        if(splits.length == 3){
-            var gameId = splits[1];
-            var nick = splits[2];
-            if(validGameId(gameId) && nickAvailable(gameId, nick)){
-                fs.readFile(__dirname + "/game.html",
-                function (err, data) {
-                    if (err) {
-                        res.writeHead(500);
-                        res.end("Error loading");
-                    }
-                    res.writeHead(200);
-                    res.end(data);
-                });
-            }else{
-                res.writeHead(500);
-                res.end("Bad game or nick");
-            }
-        }else{
-            res.writeHead(500);
-            res.end("Bad game url");
-        }
-    }else if(req['url'] == "/getgames"){
-
-        var data = [];
-        _.forEach(games, function(game){ 
-            var players=[];
-            _.forEach(game.players, function(player){
-                var obj = {nick: player.nick, score: game.scores[player.nick].score}
-                players.push(obj);
-            });
-            data.push({
-                name: game.name,
-                rounds: game.rounds,
-                players: players
-            });
-        });
-
-        data = JSON.stringify(data);
-        res.setHeader("Content-Type", "text/json");
-        res.writeHead(200);
-        res.end(data);
-    
-    } else { 
-        if(req['url'] == "/"){
-            req['url'] = "/index.html";
-        }
-        if(req['url'] == "/join"){
-            req['url'] = "/join.html";
-        }
-        fs.readFile(__dirname + req['url'],
-	        function (err, data) {
-	            if (err) {
-	                res.writeHead(500);
-	                res.end("Error loading");
-	            } else {
-	            
-		            res.setHeader("Content-Type", contentType(req['url']));
-		            res.writeHead(200);
-		            res.end(data);
-		        }
-        	}
-    	);
-    }
-}
-
-function getRandomWord(gameDifficulty) {
-    console.log(gameDifficulty);
-    if(gameDifficulty=='easy'){
-        var data = fs.readFileSync('smallwords.txt', {encoding: 'UTF8'});
-        var lines = data.split('\n');
-        return lines[Math.floor(Math.random()*lines.length)];
-    } else if(gameDifficulty=='normal'){
-        var data = fs.readFileSync('brit-a-z.txt', {encoding: 'UTF8'});
-        var lines = data.split('\n');
-        return lines[Math.floor(Math.random()*lines.length)];
-    }
-}
-
-io.sockets.on('connection', function (socket) {
-    socket.on('init', function (data){
-        console.log('sent init data', data);
-        var gameId = data.gameId;
-        var nick = data.nick;
-        socket.nick = nick;
-        var difficulty = null;
-        var ai = data.ai;
-        var speed = data.speed;
-        if(data.difficulty!=null){
-            var difficulty = data.difficulty;
-        }
-        
-        if(!games.hasOwnProperty(gameId)){
-            games[gameId] = new Game(gameId);
-        }
-        var game = games[gameId];
-        
-        if (game.join(socket, gameId, difficulty, ai, speed)){
-            console.log(nick+' joined game '+gameId);
-            var opponent;
-            if(game.players.length==2) { 
-                console.log(game.players);
-                game.players.forEach( function(player){
-                    
-                    if(player.id!=socket.id){
-                        opponent = socket.nick;
-                        player.emit('opponentJoined', opponent);
-                        socket.emit('initResponse', nick, player.nick);
-                        game.start();
-                    }
-                });
-            } else if(game.ai == true) { 
-                console.log(game.players);
-                socket.emit('initResponse', nick, 'computer');
-                game.start();
-
-            } else {
-                socket.emit('initResponse', nick, opponent);
-            }
-        }
-    });
-
-    socket.on('ask4word', function(){
-        socket.emit('wordResponse', getRandomWord());
-    });
-
-    socket.on('keyStroke', function(userText){
-        socket.game.emit(socket, 'update', userText.text);
-    });
-
-    socket.on('submitWord', function(word){
-        if(socket.game.currentWord==word){
-            socket.game.scores[socket.nick].score++;
-            socket.game.newRound(socket.nick);
-        }else{
-            //socket.emit(socket, 'wrong');
-        }
-    });
-
-});
